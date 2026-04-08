1d9c504cfe87809c39ea935ccd756b44962ca2ae
hao <2766032694@qq.com>
2026-04-08 14:49:39 +0800

    实验1

--- scripts/test-local.sh
+++ scripts/test-local.sh
@@ -1,15 +1,19 @@
 #! /usr/bin/env bash
 
-# Exit in case of error
+# 有报错，脚本立刻停止
 set -e
 
-docker-compose down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
+#停止并删除容器 -v：删除数据卷（数据库数据等） 删除“孤儿容器” 清除在出现错误后可能遗留的已损坏的堆栈信息
+# 清空上一次运行留下的所有状态
+docker-compose down -v --remove-orphans 
 
+# 删除所有 __pycache__ 目录
 if [ $(uname -s) = "Linux" ]; then
     echo "Remove __pycache__ files"
     sudo find . -type d -name __pycache__ -exec rm -r {} \+
 fi
-
+#构建镜像
 docker-compose build
+#启动容器
 docker-compose up -d
 docker-compose exec -T backend bash scripts/tests-start.sh "$@"
